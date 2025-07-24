9c1c24c79888dfe725a3272e1f4a8d58cd027098
EnzoOP1402 <enzopazian1402@gmail.com>
2025-07-24 12:26:46 -0300

    Criando o primeiro componente

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export function App() {
+    // Como em react somente um elemento pode ser retornado, sempre devemos "envelopar" o código que estamos retornando em um elemento, seja retornando uma div que contenha todos os trechos necessários, ou utilizando o React Fragment, que consiste em uma tag vazia que engloba tudo que será retornado em HtML
+    return (
+        <> 
+            <h1>Olá, mundo!</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque ipsum eum et, optio nostrum ipsam cumque! Nam libero tempora sunt quia aut eos necessitatibus impedit, eveniet cupiditate expedita accusamus nemo neque magnam alias itaque rerum, praesentium earum doloremque. Veritatis veniam minus pariatur accusantium quia impedit exercitationem quisquam ipsum dolorum, doloribus est animi quibusdam nisi dolor, maxime ea libero blanditiis nam repudiandae provident soluta sapiente temporibus, enim assumenda! Quas, saepe obcaecati dignissimos blanditiis, tempore eaque similique libero, vero facilis magni aut voluptate mollitia hic recusandae natus itaque assumenda harum pariatur eligendi porro maiores deserunt reprehenderit! Minima illum tempora nemo beatae ab.</p>
+        </>
+    )
 }
 
-export default App
+// export default App -> Como sempre precisamos exportar nossos componentes, caso não coloquemos o export antes da definção da função, precisamos inserir esse export default nessa exata posição em que ele se encontra (criar componentes através do rafce já deixa a estrutura pronta e facilita a programação). Mudanças no tipo de exportação também afetam o tipo de importação, uma vez que a exportação realizada na declaração ou a inferior com chaves faz com que seja importar o componente com seu nome entre chaves, enquanto a exportação default flexibiliza a imoortação, permitindo até uma mudança de nome durante a importação
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { App } from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+// Tags só precisam de fechamento tradicional quando els recebem elementos filhos, como ocorre com a tag <StrictMode></StrictMode>. Caso contrário, elas podem ser fechadas na própria abertura, como ocorre com a tag <App />.
